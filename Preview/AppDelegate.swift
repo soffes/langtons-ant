0b5adc7225b9112ac99744d5a4f99e1cf23eabf2
Sam Soffes <sam@soff.es>
2017-08-25 19:39:46 -0400

    Clean up preview

--- Preview/AppDelegate.swift
+++ Preview/AppDelegate.swift
@@ -1,26 +1,11 @@
 //
 //  AppDelegate.swift
-//  Preview
+//  Langton’s Ant
 //
-//  Created by Sam Soffes on 8/6/15.
-//  Copyright (c) 2015 Sam Soffes. All rights reserved.
+//  Created by Sam Soffes on 8/25/17.
+//  Copyright © 2017 Sam Soffes. All rights reserved.
 //
 
-import Cocoa
+import AppKit
 
-@NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
-	func applicationDidFinishLaunching(aNotification: NSNotification) {
-		// Insert code here to initialize your application
-	}
-
-	func applicationWillTerminate(aNotification: NSNotification) {
-		// Insert code here to tear down your application
-	}
-
-
-}
-
+@NSApplicationMain final class AppDelegate: NSObject, NSApplicationDelegate {}
--- Preview/PreviewWindowController.swift
+++ Preview/PreviewWindowController.swift
@@ -1,14 +1,14 @@
 //
 //  PreviewWindowController.swift
-//  Motivation
+//  Langton’s Ant
 //
-//  Created by Sam Soffes on 8/6/15.
-//  Copyright (c) 2015 Sam Soffes. All rights reserved.
+//  Created by Sam Soffes on 8/25/17.
+//  Copyright © 2017 Sam Soffes. All rights reserved.
 //
 
 import AppKit
 
-class PreviewWindowController: NSWindowController {
+final class PreviewWindowController: NSWindowController {
 
 	// MARK: - Properties
 
