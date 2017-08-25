024f76fe0cfcf0059eb47800fa4119bbf95e3e47
Sam Soffes <sam@soff.es>
2017-08-25 19:40:31 -0400

    More clean up

--- Langton/Langton.h
+++ Langton/Langton.h
@@ -6,14 +6,10 @@
 //  Copyright © 2017 Sam Soffes. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+@import Foundation;
 
 //! Project version number for Langton.
 FOUNDATION_EXPORT double LangtonVersionNumber;
 
 //! Project version string for Langton.
 FOUNDATION_EXPORT const unsigned char LangtonVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Langton/PublicHeader.h>
-
-
